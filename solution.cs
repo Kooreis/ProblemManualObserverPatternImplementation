9648b492202f23ba5cc2db1dccb9ee2a2e2ff620
Kooreis <kooreis.official@gmail.com>
2024-04-16 07:47:50 -0500

    Created an Observer class with a method to update and log the received data.

--- solution.cs
+++ solution.cs
@@ -1,17 +1,9 @@
-class Subject {
-  constructor() {
-    this.observers = [];
+class Observer {
+  constructor(name) {
+    this.name = name;
   }
 
-  subscribe(observer) {
-    this.observers.push(observer);
-  }
-
-  unsubscribe(observer) {
-    this.observers = this.observers.filter(obs => obs !== observer);
-  }
-
-  notify(data) {
-    this.observers.forEach(observer => observer.update(data));
+  update(data) {
+    console.log(`${this.name} received data: ${data}`);
   }
 }
